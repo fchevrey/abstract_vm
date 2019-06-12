a149eb93d66ef27e45d84f668393e61cd465a4cf
Cedrok <ced.vautrain@gmail.com>
2019-06-12 18:53:06 +0200

    add run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "test1:"
+./avm tests/test1.txt
+echo "\ntest2:"
+./avm tests/test2.txt
+echo "\ntest3:"
+./avm tests/test3.txt
+echo "\ntest4:"
+./avm tests/test4.txt
+echo "\ntest5:"
+./avm tests/test5.txt
+echo "\ntest6:"
+./avm tests/test6.txt
+echo "\ntest7:"
+./avm tests/test7.txt
+echo "\ntest8:"
+./avm tests/test8.txt
